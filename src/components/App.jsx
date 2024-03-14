9148fcfb986682631a8c285562271b79e74c85b8
Ahmed <NateRiver32000@gmail.com>
2024-03-14 15:29:07 +0200

    Notes are rendered from the notes array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+
+const createNotes = (note) =>{
+  return (
+    <Note
+    id = {note.key}
+    key = {note.key}
+    title = {note.title}
+    content = {note.content}
+    />
+  )
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
